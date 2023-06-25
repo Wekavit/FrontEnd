06da9e3cfaef02ebb1eff2c9a06b69440b2253a8
CedrickLupembe <cedricklupembe91@gmail.com>
2023-06-25 16:11:48 +0200

    Add assets and ajust metadata

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,9 @@ import { Sora } from "next/font/google";
 const sora = Sora({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Wekavit",
-  description: "Best Way To Save Your Money",
+  title: "Épargnez & investissez judicieusement votre argent - Wekavit",
+  description:
+    "Wekavit est une plateforme d'épargne en ligne très sécurisée qui permet d'épargner en alliant discipline et flexibilité pour vous faire fructifier votre épargne.",
 };
 
 export default function RootLayout({ children }) {
